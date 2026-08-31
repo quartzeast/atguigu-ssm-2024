44a3cd435bdb7b96d0a63f6b3ac59f3bd1af82fa
rockman <o_pandong@dewu.com>
2026-08-31 14:49:13 +0800

    (chore): spring 测试

--- spring/ioc/src/test/java/com/example/ioc/IocApplicationTests.java
+++ spring/ioc/src/test/java/com/example/ioc/IocApplicationTests.java
@@ -1,13 +1,34 @@
 package com.example.ioc;
 
+import com.example.ioc.dao.DeliveryDao;
+import com.example.ioc.service.UserService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
 
 @SpringBootTest
 class IocApplicationTests {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    DeliveryDao deliveryDao;
 
     @Test
     void contextLoads() {
     }
 
+    @Test
+    void test01() {
+        deliveryDao.saveDelivery();
+    }
+
+    @Test
+    void test02() {
+        String string = UUID.randomUUID().toString();
+        System.out.println("string = " + string);
+    }
 }
